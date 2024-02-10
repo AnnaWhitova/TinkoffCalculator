654f95cc6b2fc225a2cb302a2bebb60bc4919add
Anna <anya-en-belova@yandex.ru>
2024-02-10 18:39:50 +0300

    first commit

---
+++ TinkoffCalculatorTests/TinkoffCalculatorUITests.swift
@@ -0,0 +1,8 @@
+//
+//  TinkoffCalculatorUITests.swift
+//  TinkoffCalculatorTests
+//
+//  Created by Анна Белова on 04.02.2024.
+//
+
+import Foundation
---
+++ TinkoffCalculatorTests/TinkoffCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,8 @@
+//
+//  TinkoffCalculatorUITestsLaunchTests.swift
+//  TinkoffCalculatorTests
+//
+//  Created by Анна Белова on 04.02.2024.
+//
+
+import Foundation
